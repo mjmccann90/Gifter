fb3ad78979f5ff821d9106dda1ffc58eb099f224
Mark McCann <mjmccann12@me.com>
2020-07-08 11:31:53 -0500

    completed chapter

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Gifter.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gifter.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<UserProfile> UserProfile { get; set; }
+        public DbSet<Post> Post { get; set; }
     }
 }
--- Repositories/PostRepository.cs
+++ Repositories/PostRepository.cs
@@ -17,12 +17,44 @@ namespace Gifter.Repositories
 
         public List<Post> GetAll()
         {
-            return _context.Post.Include(p => p.UserProfile).ToList();
+            return _context.Post
+                .Include(p => p.UserProfile)
+                .ToList();
         }
 
         public Post GetById(int id)
         {
-            return _context.Post.FirstOrDefault(p => p.Id == id);
+            return _context.Post
+                .Include(p => p.UserProfile)
+                .FirstOrDefault(p => p.Id == id);
         }
+
+        public List<Post> GetByUserProfileId(int id)
+        {
+            return _context.Post.Include(p => p.UserProfile)
+                            .Where(p => p.UserProfileId == id)
+                            .OrderBy(p => p.Title)
+                            .ToList();
+        }
+
+        public void Add(Post post)
+        {
+            _context.Add(post);
+            _context.SaveChanges();
+        }
+
+        public void Update(Post post)
+        {
+            _context.Entry(post).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var post = GetById(id);
+            _context.Post.Remove(post);
+            _context.SaveChanges();
+        }
+
     }
 }
