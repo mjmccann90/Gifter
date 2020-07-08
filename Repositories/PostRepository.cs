06872c3762174610ad7c70390c9b872e1d26fd18
Mark McCann <mjmccann12@me.com>
2020-07-07 22:49:56 -0500

    stopping at 1:31.54

---
+++ Repositories/PostRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Gifter.Data;
+using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public class PostRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Post> GetAll()
+        {
+            return _context.Post.Include(p => p.UserProfile).ToList();
+        }
+
+        public Post GetById(int id)
+        {
+            return _context.Post.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
