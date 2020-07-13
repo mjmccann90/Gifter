9190dcac7fc4035736f34d31eb49f4c64f1e44d0
Mark McCann <mjmccann12@me.com>
2020-07-13 13:31:47 -0500

    added gitignore

---
+++ Gifter/Repositories/PostRepository.cs
@@ -0,0 +1,87 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Gifter.Data;
+using Gifter.Models;
+using System;
+
+namespace Gifter.Repositories
+{
+    public class PostRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Post> GetAll()
+        {
+            return _context.Post
+                .Include(p => p.UserProfile)
+                .Include(p => p.Comments)
+                .OrderByDescending(p => p.DateCreated)
+                .ToList();
+        }
+
+        public Post GetById(int id)
+        {
+            return _context.Post
+                .Include(p => p.UserProfile)
+                .Include(c => c.Comments)
+                .FirstOrDefault(p => p.Id == id);
+        }
+        public List<Post> Search(string criterion, bool sortDescending)
+        {
+            var query = _context.Post
+                                .Include(p => p.UserProfile)
+                                .Include(c => c.Comments)
+                                .Where(p => p.Title.Contains(criterion) ||                          p.Caption.Contains(criterion));
+
+            return sortDescending
+                ? query.OrderByDescending(p => p.DateCreated).ToList()
+                : query.OrderBy(p => p.DateCreated).ToList();
+       }
+
+        public List<Post> Hottest(DateTime dateSearched)
+        {
+            return _context.Post
+                .Include(p => p.UserProfile)
+                .Include(c => c.Comments)
+                .Where(p => p.DateCreated >= dateSearched)
+                .OrderBy(p => p.DateCreated)
+                .ToList();
+        }
+
+        public List<Post> GetByUserProfileId(int id)
+        {
+            return _context.Post.Include(p => p.UserProfile)
+                            .Include(c => c.Comments)
+                            .Where(p => p.UserProfileId == id)
+                            .OrderBy(p => p.Title)
+                            .ToList();
+        }
+
+        public void Add(Post post)
+        {
+            post.DateCreated = DateTime.Now;
+            _context.Add(post);
+            _context.SaveChanges();
+        }
+
+        public void Update(Post post)
+        {
+            _context.Entry(post).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var post = GetById(id);
+            _context.Post.Remove(post);
+            _context.SaveChanges();
+        }
+
+    }
+}
