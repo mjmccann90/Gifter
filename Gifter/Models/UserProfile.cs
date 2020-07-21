8c90c55b8aa40ed0895ad0eafd230b37089a24f8
Mark McCann <mjmccann12@me.com>
2020-07-21 09:56:18 -0500

    added usertype and modiefied apdbcontext and userpro

--- Gifter/Models/UserProfile.cs
+++ Gifter/Models/UserProfile.cs
@@ -11,13 +11,23 @@ namespace Gifter.Models
         [Required]
         public string Name { get; set; }
 
+        [StringLength(28, MinimumLength = 28)]
+        public string FirebaseUserId { get; set; }
+
         [Required]
+        [EmailAddress]
+        [StringLength(255, MinimumLength = 5)]
         public string Email { get; set; }
 
         public string ImageUrl { get; set; }
 
         [Required]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
         public DateTime DateCreated { get; set; }
+
+        [Required]
+        public int UserTypeId { get; set; }
+        public UserType UserType { get; set; }
 
     }
 }
